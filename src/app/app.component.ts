b52dce915351550d5edad4066cef1df7c4eedba5
Fabio <fabioaoad@gmail.com>
2021-12-19 13:17:57 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dados';
+  dadoIzquierda = '../assets/img/dado1.png';
+  dadoDerecha   = '../assets/img/dado4.png';
+
+  numero1 = 1;
+  numero2 = 2;
+
+  tirarDados(){
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+   // console.log(this.numero1);
+   // console.log(this.numero2);
+    this.dadoIzquierda = `../assets/img/dado${this.numero1}.png`;
+    this.dadoDerecha   = `../assets/img/dado${this.numero2}.png`;
+
+  }
+
 }
